76d80f14eeb797b7cf86317df22de895038bc319
Matvey Makushenko <matvey.myown@gmail.com>
2025-05-08 17:43:03 +0300

    Solve all tasks

--- CloneProject/task04.cpp
+++ CloneProject/task04.cpp
@@ -35,6 +35,14 @@
 *	[output 4]: false
 */
 
-bool task04(int x1, int y1, int x2, int y2) {
-	return false;
+bool task04(int x1, int y1, int x2, int y2)
+{
+	if (x1 <= 0 || y1 <= 0 || x2 <= 0 || y2 <= 0
+		|| x1 > 8 || y1 > 8 || x2 > 8 || y2 > 8
+		|| (x1 == x2 && y1 == y2))
+	{
+		return false;
+	}
+
+	return x1 == x2 || y1 == y2;
 }
